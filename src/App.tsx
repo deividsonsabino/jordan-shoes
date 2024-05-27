aa728d4d5be345ae75777c97b20680b19d2d5d2f
deividsonsabino <deividsonsabino@outlook.com.br>
2024-05-27 20:49:53 -0300

    ⚙️refactor: Scripts and folders in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Fragment } from 'react/jsx-runtime'
+import HomePage from './pages'
 
 function App() {
 
   return (
     <Fragment>
-      Hello World 🎉
+      <HomePage />
     </Fragment>
   )
 }
